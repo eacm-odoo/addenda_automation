ba34d448f956039736ba8f3498722fa604b24908
EduardoCedillo(eacm) <eacm@odoo>
2022-07-08 21:49:56 +0000

    [ADD] addenda_automated: inherit from res.partner to add the checkbox that checks which addenda will be use

--- odoo_addenda/models/res_partner.py
+++ odoo_addenda/models/res_partner.py
@@ -7,3 +7,10 @@ class ResPartner(models.Model):
     addenda_addenda = fields.Many2one(
         comodel_name="addenda.addenda", string="Addenda Customed created by User")
     is_customed_addenda = fields.Boolean(string="Is Customed Addenda")
+
+    @api.onchange('is_customed_addenda')
+    def onchange_is_customed_addenda(self):
+        if self.is_customed_addenda:
+            self.l10n_mx_edi_addenda = False
+        else:
+            self.addenda_addenda = False
