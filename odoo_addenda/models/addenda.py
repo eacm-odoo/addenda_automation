ab76c95d3ce6ad564bcb8675bc4b55af3effcf48
EduardoCedillo(eacm) <eacm@odoo.com>
2022-07-15 16:52:22 -0500

    [ADD] addenda_automated: MVP4 start development add the fields tag_name and addenda_tag_ids in addenda.addenda

--- odoo_addenda/models/addenda.py
+++ odoo_addenda/models/addenda.py
@@ -1,43 +1,54 @@
 import xml.etree.ElementTree as ET
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+from lxml import etree
 
 
 class AddendaAddenda(models.Model):
     _name = 'addenda.addenda'
     _description = 'Automated addenda'
 
-    name = fields.Char(string='Name', required=True,help=_('The name of the new customed addenda'))
-    nodes_ids = fields.One2many(comodel_name='addenda.node', string='Nodes', inverse_name='addenda_id')
+    name = fields.Char(string='Name', required=True, help=_(
+        'The name of the new customed addenda'))
+    nodes_ids = fields.One2many(
+        comodel_name='addenda.node', string='Nodes', inverse_name='addenda_id')
     is_customed_addenda = fields.Boolean(string='Customed Addenda', )
-    expression = fields.Text(string='Expression')
+    addenda_tag_id = fields.One2many(
+        string='Addenda Tags', comodel_name='addenda.tag', inverse_name='addenda_addenda_id', help=_('New addenda tags added'))
+    tag_name = fields.Char(string='Root Tag Name', required=True,
+                           help=_('Name of the root tag tree'))
 
-    @api.onchange('expression')
-    def evalue_expression(self):
-        for node in self:
-            if(node.expression):
-                try:
-                    ET.fromstring(node.expression)
-                except:
-                    raise UserError(_("invalid format for xml"))
-                    
     @api.model
     def create(self, vals_list):
         res = super().create(vals_list)
         if not(vals_list['is_customed_addenda']):
-            
+            root = etree.Element(vals_list['tag_name'])
+            for tag in res.addenda_tag_id:
+                child_tag = self.generate_tree(tag)
+                root.append(child_tag)
+            root_string = etree.tostring(root, pretty_print=True)
+            print(root_string)
             ir_ui_view = self.env['ir.ui.view'].create({
                 'name': vals_list['name'],
                 'type': 'qweb',
-                'arch': vals_list['expression'],
+                'arch': root_string,
                 'active': True,
                 'inherit_id': False,
                 'model': False,
                 'priority': 16,
-                'arch_base': vals_list['expression'],
-                'arch_db': vals_list['expression'],
+                'arch_base': root_string,
+                'arch_db': root_string,
                 'arch_fs': False,
                 'mode': 'primary',
                 'l10n_mx_edi_addenda_flag': True,
             })
         return res
+
+    def generate_tree(self, addenda_tag):
+        parent_node = etree.Element(addenda_tag.tag_name)
+        if addenda_tag.addenda_tag_childs_ids:
+            for child in addenda_tag.addenda_tag_childs_ids:
+                child_node = self.generate_node(child)
+                parent_node.append(child_node)
+
+        return parent_node
--- odoo_addenda/models/addenda_tag.py
+++ odoo_addenda/models/addenda_tag.py
@@ -13,61 +13,64 @@ class AddendaTag(models.Model):
 
     addenda_node_id = fields.Many2one(
         string='Addenda Node', comodel_name='addenda.node')
+    addenda_addenda_id = fields.Many2one(
+        string='Addenda Addenda', comodel_name='addenda.addenda')
     addenda_tag_childs_ids = fields.One2many(
         comodel_name='addenda.tag', string='Addenda Tag Childs', inverse_name='addenda_tag_id', help=_('New elements added inside this tag/element'))
     addenda_tag_id = fields.Many2one(
         string='Addenda tag', comodel_name='addenda.tag')
-    tag_name = fields.Char(string='Tag Name', required=True, help=_('Name of the new tag/element'))
-    attribute = fields.Char(string='Attribute', help=_('Name of the attribute of the new element'))
-    value = fields.Char(string='Attribute Value', help=_('Value of the attribute of the new element'))
+    tag_name = fields.Char(string='Tag Name', required=True,
+                           help=_('Name of the new tag/element'))
+    attribute = fields.Char(string='Attribute', help=_(
+        'Name of the attribute of the new element'))
+    value = fields.Char(string='Attribute Value', help=_(
+        'Value of the attribute of the new element'))
     field = fields.Many2one(
-        string='Field', help=_('The value that will appear on the invoice once generated'), comodel_name='ir.model.fields', 
-    domain=[('model', '=', 'account.move'),('ttype', 'in',('char','text','selection','many2one', 'monetary', 'integer', 'boolean', 'date', 'datetime'))])
+        string='Field', help=_('The value that will appear on the invoice once generated'), comodel_name='ir.model.fields',
+        domain=[('model', '=', 'account.move'), ('ttype', 'in', ('char', 'text', 'selection', 'many2one', 'monetary', 'integer', 'boolean', 'date', 'datetime'))])
     inner_field = fields.Many2one(
         string='Inner field', help=_(''), comodel_name='ir.model.fields')
-    preview= fields.Text(store=False, string='Preview',readonly=True,compute='_compute_preview')
-    
+    preview = fields.Text(store=False, string='Preview',
+                          readonly=True, compute='_compute_preview')
 
-    
     @api.onchange('field')
     def _compute_inner_fields(self):
         domain = {'inner_field': []}
         for record in self:
             if record.field.ttype == 'many2one':
-                domain = {'inner_field': [('model', '=', record.field.relation),('ttype','not in',('many2one'))]}
+                domain = {'inner_field': [
+                    ('model', '=', record.field.relation), ('ttype', 'not in', ('many2one'))]}
         return {'domain': domain}
-    
+
     @api.onchange('addenda_tag_childs_ids')
     def _remove_field(self):
         for record in self:
-            if len(record.addenda_tag_childs_ids)>0:
+            if len(record.addenda_tag_childs_ids) > 0:
                 record.field = False
-                
+
     @api.onchange('field')
     def _remove_child_tags(self):
         for record in self:
             if record.field:
-                record.addenda_tag_childs_ids=False
-    
+                record.addenda_tag_childs_ids = False
+
     @api.onchange('value')
     def _remove_field(self):
         for record in self:
             if record.value:
-                record.field=False
+                record.field = False
 
-    @api.depends('tag_name','attribute','value','field','addenda_tag_childs_ids')
+    @api.depends('tag_name', 'attribute', 'value', 'field', 'addenda_tag_childs_ids')
     def _compute_preview(self):
-        tag=self.tag_name or 'Tag name'
-        attr=self.attribute or ''
-        value= (self.field.name or self.value) or 'Value'
+        tag = self.tag_name or 'Tag name'
+        attr = self.attribute or ''
+        value = (self.field.name or self.value) or 'Value'
         childs = ""
         if self.addenda_tag_childs_ids:
             for child_tag in self.addenda_tag_childs_ids:
-                childs =childs + '\n\t<' + child_tag.tag_name + '>'
+                childs = childs + '\n\t<' + child_tag.tag_name + '>'
             childs = childs + '\n'
         #childs= '\n\t<child> </child>\n\t.\n\t.\n\t.\n' if len(self.addenda_tag_childs_ids) >0 else ''
-        
-        self.preview=("<cfdi:%s %s> %s </cfdi:%s>"% (tag,('%s=%s'%(attr,value)) if attr !='' else '',childs,tag))
-    
-        
-            
+
+        self.preview = ("<cfdi:%s %s> %s </cfdi:%s>" % (tag, ('%s=%s' %
+                        (attr, value)) if attr != '' else '', childs, tag))
