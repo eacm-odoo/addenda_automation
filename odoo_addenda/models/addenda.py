fa0bc4af9a108a952498ba28c8f5484420b10f85
EduardoCedillo(eacm) <eacm@odoo.com>
2022-07-18 15:43:50 -0500

    [ADD] addenda_automated: zip file created with the addenda_view, now can export the zip file

--- odoo_addenda/models/addenda.py
+++ odoo_addenda/models/addenda.py
@@ -1,4 +1,7 @@
 import xml.etree.ElementTree as ET
+import os
+import base64
+from shutil import make_archive, rmtree
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 from lxml import etree
@@ -17,6 +20,11 @@ class AddendaAddenda(models.Model):
         string='Addenda Tags', comodel_name='addenda.tag', inverse_name='addenda_addenda_id', help=_('New addenda tags added'))
     tag_name = fields.Char(string='Root Tag Name', required=True,
                            help=_('Name of the root tag tree'))
+    state = fields.Selection(string="State", selection=[
+        ('draft', "Draft"),
+        ('done', "Done")
+    ], default='draft')
+    addenda_xml = fields.Text(string='Addenda XML', help=_('Addenda XML'))
 
     @api.model
     def create(self, vals_list):
@@ -26,8 +34,8 @@ class AddendaAddenda(models.Model):
             for tag in vals_list['addenda_tag_id']:
                 xml_tree_tag = self.generate_tree_view(tag[2])
                 root.append(xml_tree_tag)
+            full_xml = self.get_xml(vals_list['name'], root)
             root_string = etree.tostring(root, pretty_print=True)
-            print(root_string)
             ir_ui_view = self.env['ir.ui.view'].create({
                 'name': vals_list['name'],
                 'type': 'qweb',
@@ -42,13 +50,14 @@ class AddendaAddenda(models.Model):
                 'mode': 'primary',
                 'l10n_mx_edi_addenda_flag': True,
             })
+            res.write({'state': 'done', 'addenda_xml': etree.tostring(
+                full_xml, pretty_print=True)})
         return res
 
     # Function to create the xml tree, given  tag_name and addenda_tag_id
 
     def generate_tree_view(self, addenda_tag):
         if type(addenda_tag) is list:
-            print(addenda_tag)
             addenda_tag = addenda_tag[2]
             parent_node = etree.Element(addenda_tag['tag_name'])
         else:
@@ -81,3 +90,85 @@ class AddendaAddenda(models.Model):
 
     def get_field_name(self, field_id):
         return self.env['ir.model.fields'].browse(field_id).name
+
+    def get_xml(self, name, root):
+        xml = etree.Element("odoo")
+        xml.set("noupdate", "0")
+        template = etree.Element("template")
+        template.set("id", "l10n_mx_edi_addenda_{}".format(
+            name.lower().replace(' ', '_')))
+        template.set("name", name)
+        template.append(root)
+        xml.append(template)
+        record = etree.Element("record")
+        record.set("id", "l10n_mx_edi_addenda_{}".format(
+            name.lower().replace(' ', '_')))
+        record.set("model", "ir.ui.view")
+        field = etree.Element("field")
+        field.set("name", "l10n_mx_edi_addenda_flag")
+        field.text = "True"
+        record.append(field)
+        xml.append(record)
+        return xml
+
+    def action_export_zip(self):
+        zip_file = self.create_directory(
+            self.name, etree.fromstring(self.addenda_xml))
+        attachment = self.env['ir.attachment'].create({
+            'name': 'export_zip_module',
+            'type': 'binary',
+            'datas': zip_file,
+            'mimetype': 'application/zip',
+            'description': _('Zip file with the addenda and the new fields'),
+        })
+        rmtree(self.name)
+        os.remove('addenda.zip')
+        return {
+            'target': 'new',
+            'type': 'ir.actions.act_url',
+            'url': '/web/content/%s?download=1' % attachment['id']
+        }
+
+    def create_directory(sefl, name, xml):
+        template = {
+            'name': name,
+            'sumary': 'Addenda created using addenda.addenda',
+            'description': "",
+            'author': 'Odoo PS',
+            'category': 'Sales',
+            'version': '15.0.1.0.0',
+            'depends': ['sale', 'l10n_mx_edi'],
+            'license': 'OPL-1',
+            'data': [
+                'views/addendas.xml',
+            ],
+        }
+
+        os.makedirs(name+"/"+name+"/views")
+        f = open(name+"/"+name+"/__manifest__.py", "w")
+        f.write('{\n')
+        for key, value in template.items():
+            if(type(value) == list):
+                f.write("'%s' : %s,\n" % (key, value))
+            else:
+                f.write("'%s' : '%s',\n" % (key, value))
+        f.write('}')
+        f.close()
+        f = open(name+"/"+name+"/__init__.py", "w")
+        f.close()
+        tree = etree.ElementTree(xml)
+        # save xml in the folder views
+        tree.write(name+"/"+name+"/views/addendas.xml",
+                   pretty_print=True, xml_declaration=True, encoding='utf-8')
+        f.close()
+        make_archive(
+            'addenda',
+            'zip',           # the archive format - or tar, bztar, gztar
+            name)
+
+        f = open("addenda.zip", "rb")
+        bytes_content = f.read()
+
+        bytes_content = base64.encodebytes(bytes_content)
+        # rmtree("name")
+        return bytes_content
