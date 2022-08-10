2656f6b430df946502ae7535842d8e3a4e5376ac
hmpb-odoo <hmpb@odoo.com>
2022-08-10 09:53:34 -0500

    [IMP] addenda_automation: Test for creation of the addenda with a tree tag, and a tree tag with a created field

--- odoo_addenda/tests/test_addenda.py
+++ odoo_addenda/tests/test_addenda.py
@@ -32,3 +32,43 @@ class TestAddendaAutomationResults(TestAddendaAutomation):
                          b'<Initial>\n  <OrdenCompra>\n    <t t-esc="record.name"/>\n  </OrdenCompra>\n  <Partner>\n    <t t-esc="record.partner_id.name"/>\n  </Partner>\n  <Attribute addendaTestValue="testValue" t-att-addendaTestField="record.name" t-att-addendaTestInnerField="record.partner_id.name"/>\n</Initial>\n')
 
         print(generate_tree_view)
+
+    def test_create_addenda_with_tree_tag(self):
+        addenda = self.env['addenda.addenda'].create({
+            'name': 'Addenda for Barry',
+            'is_expression': False,
+            'main_preview': False,
+            'is_customed_addenda': False,
+            'tag_name': 'Initial',
+            'fields': [],
+            'addenda_tag_id': [(6, 0, self.tag_initial.id)]
+        })
+        self.assertTrue(addenda)
+
+        #Computed fields for addenda
+        self.assertEqual(addenda.main_preview, 
+                         '<Initial>\n    <Initial>\n        <OrdenCompra>\n            <t t-esc="record.name"/>\n        </OrdenCompra>\n        <Partner>\n            <t t-esc="record.partner_id.name"/>\n        </Partner>\n        <Attribute t-att-addendaTestValue="testValue" t-att-addendaTestField="record.name" t-att-addendaTestInnerField="record.partner_id.name"/>\n    </Initial>\n</Initial>\n')
+
+        self.assertEqual(addenda.addenda_tag_id[0].preview,
+                            '<Initial>\n    <OrdenCompra>\n        <t t-esc="record.name"/>\n    </OrdenCompra>\n    <Partner>\n        <t t-esc="record.partner_id.name"/>\n    </Partner>\n    <Attribute t-att-addendaTestValue="testValue" t-att-addendaTestField="record.name" t-att-addendaTestInnerField="record.partner_id.name"/>\n</Initial>\n')
+        
+        self.assertEqual(addenda.addenda_tag_id[0].addenda_tag_childs_ids[0].field_type, 'char')
+
+        self.assertEqual(str(addenda.addenda_tag_id[0].len_tag_childs), '3')
+
+        self.assertEqual(addenda.addenda_tag_id[0].addenda_tag_childs_ids[2].attribute_ids[1].field_type, 'char')
+
+    def test_create_addenda_with_tree_tag_and_field(self):
+        addenda = self.env['addenda.addenda'].create({
+            'name': 'Addenda for Barry',
+            'is_expression': False,
+            'main_preview': False,
+            'is_customed_addenda': False,
+            'tag_name': 'Initial',
+            'fields': [],
+            'addenda_tag_id': [(6, 0, self.tree_tag_with_created_field.id)]
+        })
+
+        self.assertTrue(addenda)
+
+
